550bfb018a20a7c146ba99a197ef534c7f7ce525
@YuliMir <juliya.mironovav@gmail.com>
2022-06-14 20:38:08 +0300

    Initial Commit

---
+++ All for fun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All for fun
+//
+//  Created by Юлия Миронова on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
